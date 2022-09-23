bef17386b96611915246f209d691a5628a795b4a
PRIYABRATAM9 <114126709+PRIYABRATAM9@users.noreply.github.com>
2022-09-23 17:55:42 +0530

    Add files via upload

---
+++ FINAL PROJECT.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# "# My Jupyter Notebook on IBM Watson Studio"
+# 
+
+# __PRIYABRATA MOHAPATRA__
+# STUDENT
+
+# _I am interested in data science because it enables companies to efficiently understand gigantic data from multiple sources and derive valuable insights to make smarter data-driven decisions. Data Science is widely used in various industry domains, including marketing, healthcare, finance, banking, policy work, and more.
+
+# "### The next cell will calculate addition to two numbers"
+
+# In[1]:
+
+
+x = 5
+y = 10
+sum = int(x) + int(y)
+print("The sum is: ", sum)
+
+
+# ***hii whats up
+# 1. MY
+# 2. NAME
+# 3. PITU
+# - END NOW -
